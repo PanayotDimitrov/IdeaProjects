d41e5c7332084d6beb58a76d4aec00f56c518224
Panayot Dimitrov <paceto2014@gmail.com>
2023-09-23 21:02:26 +0300

    Multidimensional Arrays

---
+++ Advanced/src/MultidimensionalArrays/MatrixOfPalindromes.java
@@ -0,0 +1,44 @@
+package MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class MatrixOfPalindromes {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String [] tokens = scanner.nextLine().split("\\s+");
+        int r = Integer.parseInt(tokens[0]);
+        int c = Integer.parseInt(tokens[1]);
+
+        String [][] matrix = new String[r][c];
+
+        fillPalindromeMatrix(matrix);
+        printMatrix(matrix);
+
+    }
+
+
+    private static void fillPalindromeMatrix(String[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+
+                String outsideSymbol = String.valueOf((char) ('a' + row));
+                String midSymbol = String.valueOf((char) ('a'+row +col));
+                matrix[row][col] = outsideSymbol + midSymbol + outsideSymbol;
+
+
+            }
+        }
+    }
+    private static void printMatrix(String[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ Advanced/src/MultidimensionalArrays/MaximalSum.java
@@ -0,0 +1,56 @@
+package MultidimensionalArrays;
+import java.util.Scanner;
+
+
+public class MaximalSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Read the dimensions of the matrix
+        int N = input.nextInt();
+        int M = input.nextInt();
+
+        // Read the matrix
+        int[][] matrix = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        // Initialize variables to keep track of the maximum sum and the coordinates of the top-left corner
+        int maxSum = Integer.MIN_VALUE;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        // Find the square with the maximal sum
+        for (int i = 0; i <= N - 3; i++) {
+            for (int j = 0; j <= M - 3; j++) {
+                int squareSum = 0;
+                for (int row = i; row < i + 3; row++) {
+                    for (int col = j; col < j + 3; col++) {
+                        squareSum += matrix[row][col];
+                    }
+                }
+                if (squareSum > maxSum) {
+                    maxSum = squareSum;
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
+// Print the sum of the elements in the square
+        System.out.println("Sum = " + maxSum);
+
+        // Print the 3x3 square with the maximal sum
+        for (int i = maxRow; i < maxRow + 3; i++) {
+            for (int j = maxCol; j < maxCol + 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        input.close();
+    }
+}
