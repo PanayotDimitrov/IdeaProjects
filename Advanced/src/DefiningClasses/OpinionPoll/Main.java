315d00a92fb0c53e555ddff65c9da9b9ffb77e20
Panayot Dimitrov <paceto2014@gmail.com>
2023-10-06 10:38:30 +0300

    Program
    Have to sort names in "Main".

---
+++ Advanced/src/DefiningClasses/OpinionPoll/Main.java
@@ -0,0 +1,41 @@
+package DefiningClasses.OpinionPoll;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Person> allPeople = new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            String personName = tokens[0];
+            int personAge = Integer.parseInt(tokens[1]);
+
+            Person person = new Person(personName, personAge);
+
+            allPeople.add(person);
+        }
+
+
+        for (int i = 0; i < allPeople.size(); i++) {
+
+            if (allPeople.get(i).getAge() >= 30) {
+
+
+                //da sortiram imenata vuv vuzhodqsht red
+                
+                System.out.println(allPeople.get(i));
+            }
+        }
+    }
+}
