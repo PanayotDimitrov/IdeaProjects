8f330fa7a2a04495ad4c813a2a87ba88b453c97f
Panayot Dimitrov <paceto2014@gmail.com>
2023-10-21 14:29:41 +0300

    changing packages

---
+++ Advanced/src/functional_programming/ConsumerPrint.java
@@ -0,0 +1,19 @@
+package functional_programming;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class ConsumerPrint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> print = (s) -> System.out.println(s);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(print);
+
+    }
+}
---
+++ Advanced/src/generics/Demo.java
@@ -0,0 +1,12 @@
+package generics;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
